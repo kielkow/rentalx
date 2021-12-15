a3c509a80169354b95bcd83690be248ce106d79b
kielkow <matheuskiel@fiorifer.com.br>
2021-12-15 14:48:10 -0300

    refactor: inject createSpecification dependecies with tsyringe

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 
-import createSpecificationController from '../modules/cars/useCases/createSpecification';
+import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 import listSpecificationsController from '../modules/cars/useCases/listSpecifications';
 
 const specificationsRoutes = Router();
+
+const createSpecificationController = new CreateSpecificationController();
 
 specificationsRoutes.get('/', (request, response) => {
   return listSpecificationsController().handle(request, response);
 });
 
-specificationsRoutes.post('/', (request, response) => {
-  return createSpecificationController().handle(request, response);
-});
+specificationsRoutes.post('/', createSpecificationController.handle);
 
 export { specificationsRoutes };
