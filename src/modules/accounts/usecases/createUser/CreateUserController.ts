a638137952aa601fb53274c45084d0367e409c8a
kielkow <matheuskiel@fiorifer.com.br>
2021-12-27 16:12:47 -0300

    refactor: remove column username from users table

--- src/modules/accounts/usecases/createUser/CreateUserController.ts
+++ src/modules/accounts/usecases/createUser/CreateUserController.ts
@@ -6,13 +6,12 @@ import { CreateUserUseCase } from './CreateUserUseCase';
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     try {
-      const { name, username, password, email, driver_license } = request.body;
+      const { name, password, email, driver_license } = request.body;
 
       const createUserUseCase = container.resolve(CreateUserUseCase);
 
       await createUserUseCase.execute({
         name,
-        username,
         password,
         email,
         driver_license,
