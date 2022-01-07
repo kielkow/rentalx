0111a28891bd6b9475c0f83b7d5dd06c76c5947e
kielkow <matheuskiel@fiorifer.com.br>
2022-01-07 13:49:53 -0300

    feat: create AuthenticateUserUseCase

---
+++ src/modules/accounts/usecases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,57 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw { status: 400, message: 'E-mail or password incorrect' };
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw { status: 400, message: 'E-mail or password incorrect' };
+    }
+
+    const token = sign({}, 'd2e3a40eae0ed07a8a26c51cbf0d3d34', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const authenticateInfo = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+
+    return authenticateInfo;
+  }
+}
+
+export { AuthenticateUserUseCase };
