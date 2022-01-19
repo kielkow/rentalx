1ff7e25e53b97626a31b6dce66a27e7192591e9f
kielkow <matheuskiel@fiorifer.com.br>
2022-01-19 12:18:58 -0300

    test: testing jest execution

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe('Create Category', () => {
+  it('Result should be equal a 4', () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it('Result should not be equal a 4', () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
