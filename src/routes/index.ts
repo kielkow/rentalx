e7e861bc13c481dd740f5d490a5f63dff5fb0da8
kielkow <matheuskiel@fiorifer.com.br>
2022-01-25 12:27:39 -0300

    refactor: refact routes imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from 'middlewares/ensureAuthenticated';
 
-import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
