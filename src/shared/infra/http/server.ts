8111801ad8ccdd5bfcab688ca866b15411bb1056
kielkow <matheuskiel@fiorifer.com.br>
2022-01-27 08:19:28 -0300

    refactor: move database to shared module

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -6,7 +6,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swagger from 'swagger-ui-express';
 
-import '../../../database';
+import '../typeorm';
 
 import '@shared/container';
 
