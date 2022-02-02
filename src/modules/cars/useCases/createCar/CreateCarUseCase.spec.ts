586a52f91b856c0bd10b9ee661aced8b54e5ec81
kielkow <matheuskiel@fiorifer.com.br>
2022-02-02 09:29:40 -0300

    test: CreateCarUseCase should not be able to create a car with exists license plate

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarUseCase } from './CreateCarUseCase';
 
@@ -21,5 +22,29 @@ describe('Create Car', () => {
       brand: 'car brand',
       category_id: 'category_id',
     });
+  });
+
+  it('should not be able to create a car with exists license plate', async () => {
+    await createCarUseCase.execute({
+      name: 'car name',
+      description: 'car description',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'car brand',
+      category_id: 'category_id',
+    });
+
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'another car name',
+        description: 'another car description',
+        daily_rate: 150,
+        license_plate: 'ABC-1234',
+        fine_amount: 80,
+        brand: 'another car brand',
+        category_id: 'category_id',
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
