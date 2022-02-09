a9d0d852deb64f216d74438d0835dc0b6988cb21
kielkow <matheuskiel@fiorifer.com.br>
2022-02-09 12:49:18 -0300

    test: ListCarsUseCase should be able to list all available cars

---
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -0,0 +1,29 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+
+import { ListCarsUseCase } from './ListCarsUseCase';
+
+let listCarsUseCase: ListCarsUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('List Cars', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    listCarsUseCase = new ListCarsUseCase(carsRepositoryInMemory);
+  });
+
+  it('should be able to list all available cars', async () => {
+    const carAvailable = await carsRepositoryInMemory.create({
+      name: 'car available',
+      description: 'car description',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'car brand',
+      category_id: 'category_id',
+    });
+
+    const cars = await listCarsUseCase.execute();
+
+    expect(cars).toEqual([carAvailable]);
+  });
+});
