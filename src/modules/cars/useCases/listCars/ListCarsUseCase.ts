f8824427837ff4c69f751b45cfc1900ad4db801c
kielkow <matheuskiel@fiorifer.com.br>
2022-02-10 11:55:33 -0300

    feat: receive params on listCars usecase

--- src/modules/cars/useCases/listCars/ListCarsUseCase.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.ts
@@ -3,6 +3,12 @@ import { injectable, inject } from 'tsyringe';
 import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 
+interface IRequest {
+  category_id?: string;
+  brand?: string;
+  name?: string;
+}
+
 @injectable()
 class ListCarsUseCase {
   constructor(
@@ -10,8 +16,12 @@ class ListCarsUseCase {
     private carsRepository: ICarsRepository,
   ) {}
 
-  async execute(): Promise<Car[]> {
-    const cars = await this.carsRepository.findAvailable();
+  async execute({ category_id, brand, name }: IRequest): Promise<Car[]> {
+    const cars = await this.carsRepository.findAvailable(
+      category_id,
+      brand,
+      name,
+    );
 
     return cars;
   }
