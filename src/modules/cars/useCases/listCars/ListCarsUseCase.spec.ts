6ad9455a87a4dc28db4327340720c09e38a8cec5
kielkow <matheuskiel@fiorifer.com.br>
2022-02-10 11:56:16 -0300

    test: listCarsUseCase should be able to list all available cars by name

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -22,8 +22,36 @@ describe('List Cars', () => {
       category_id: 'category_id',
     });
 
-    const cars = await listCarsUseCase.execute();
+    const cars = await listCarsUseCase.execute({});
 
     expect(cars).toEqual([carAvailable]);
+  });
+
+  it('should be able to list all available cars by name', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'car available',
+      description: 'car description',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'car brand',
+      category_id: 'category_id',
+    });
+
+    await carsRepositoryInMemory.create({
+      name: 'another car available',
+      description: 'car description',
+      daily_rate: 100,
+      license_plate: 'ABC-12345',
+      fine_amount: 60,
+      brand: 'car brand',
+      category_id: 'category_id',
+    });
+
+    const cars = await listCarsUseCase.execute({
+      name: 'car available',
+    });
+
+    expect(cars).toEqual([car]);
   });
 });
