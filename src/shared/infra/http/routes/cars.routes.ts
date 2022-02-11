65739a997fbb41a168dba3078bbf34b7efeefc8a
kielkow <matheuskiel@fiorifer.com.br>
2022-02-11 11:16:29 -0300

    feat: create cars routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
-carsRoutes.post('/', ensureAdmin, createCarController.handle);
+const createCarRoute = carsRoutes.post(
+  '/',
+  ensureAdmin,
+  createCarController.handle,
+);
 
-export { carsRoutes };
+const listCarsRoute = carsRoutes.get(
+  '/available',
+  listAvailableCarsController.handle,
+);
+
+export { createCarRoute, listCarsRoute };
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 import { authenticateRoutes } from './authenticate.routes';
-import { carsRoutes } from './cars.routes';
+import { createCarRoute, listCarsRoute } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
@@ -12,10 +12,11 @@ const router = Router();
 
 router.use(authenticateRoutes);
 router.use('/users', usersRoutes);
+router.use('/cars', listCarsRoute);
 
 router.use(ensureAuthenticated);
 
-router.use('/cars', carsRoutes);
+router.use('/cars', createCarRoute);
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 
