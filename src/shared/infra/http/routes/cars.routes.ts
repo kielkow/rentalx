92c0e888a6bce96ad9ef5a4ac93a6178501cc68e
kielkow <matheuskiel@fiorifer.com.br>
2022-02-11 11:43:00 -0300

    fix: fix createCarsRoute

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,18 +3,12 @@ import { Router } from 'express';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 
-import { ensureAdmin } from '../middlewares/ensureAdmin';
-
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 
-const createCarRoute = carsRoutes.post(
-  '/',
-  ensureAdmin,
-  createCarController.handle,
-);
+const createCarRoute = carsRoutes.post('/', createCarController.handle);
 
 const listCarsRoute = carsRoutes.get(
   '/available',
