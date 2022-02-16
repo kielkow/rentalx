23b0e487d3a8045014c8c01e7a200f9fcde7eef8
kielkow <matheuskiel@fiorifer.com.br>
2022-02-16 10:09:15 -0300

    test: add SpecificationsRepository to tests

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,16 +1,21 @@
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { SpecificationsRepositoryInMemory } from '@modules/cars/repositories/in-memory/SpecificationRepositoryInMemory';
 import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
 let carsRepositoryInMemory: CarsRepositoryInMemory;
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory;
 
 describe('Create Car Specification', () => {
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory();
+    specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory();
+
     createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
       carsRepositoryInMemory,
+      specificationsRepositoryInMemory,
     );
   });
 
@@ -25,11 +30,17 @@ describe('Create Car Specification', () => {
       category_id: 'category_id',
     });
 
-    const specifications_id = ['specification_id'];
+    await specificationsRepositoryInMemory.create({
+      name: 'specification_name',
+      description: 'specification_description',
+    });
+    const specification = await specificationsRepositoryInMemory.findByName(
+      'specification_name',
+    );
 
     await createCarSpecificationUseCase.execute({
       car_id: car.id,
-      specifications_id,
+      specifications_id: [specification.id],
     });
   });
 
