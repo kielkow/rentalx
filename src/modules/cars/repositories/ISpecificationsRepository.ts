0da90431c44016d94019bcedcbdc87db474751d5
kielkow <matheuskiel@fiorifer.com.br>
2022-02-16 10:07:05 -0300

    feat: add method findByIds to SpecificationsRepository

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -9,6 +9,7 @@ interface ISpecificationsRepository {
   list(): Promise<Specification[]>;
   create({ name, description }: ICreateSpecificationDTO): Promise<void>;
   findByName(name: string): Promise<Specification>;
+  findByIds(ids: string[]): Promise<Specification[]>;
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
