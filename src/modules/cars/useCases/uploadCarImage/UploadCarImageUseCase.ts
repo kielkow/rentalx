e8da8076f5050e0f81473a81d8ea6836bee5bad1
kielkow <matheuskiel@fiorifer.com.br>
2022-02-19 19:01:41 -0300

    feat: add uploadCarImageUseCase

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,10 +1,12 @@
 import { inject, injectable } from 'tsyringe';
 
+import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
   car_id: string;
-  image_name: string;
+  images_name: string[];
 }
 
 @injectable()
@@ -12,10 +14,21 @@ class UploadCarImageUseCase {
   constructor(
     @inject('CarsRepository')
     private carsRepository: ICarsRepository,
+
+    @inject('CarsImagesRepository')
+    private carsImagesRepository: ICarsImagesRepository,
   ) {}
 
-  async execute({ car_id, image_name }: IRequest): Promise<void> {
-    console.log(car_id, image_name);
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    const carExists = await this.carsRepository.findById(car_id);
+
+    if (!carExists) {
+      throw new AppError('Car does not exists');
+    }
+
+    images_name.map(async image => {
+      await this.carsImagesRepository.create(car_id, image);
+    });
   }
 }
 
