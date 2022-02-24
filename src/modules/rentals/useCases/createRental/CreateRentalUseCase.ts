a0d6932cbe0bab572135abe3b4a1c3dea1a0a6cd
kielkow <matheuskiel@fiorifer.com.br>
2022-02-24 10:51:17 -0300

    feat: create rental on CreateRentalUseCase

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,4 @@
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
 import { AppError } from '@shared/errors/AppError';
 
@@ -14,11 +15,10 @@ class CreateRentalUseCase {
     user_id,
     car_id,
     expected_return_date,
-  }: IRequest): Promise<void> {
+  }: IRequest): Promise<Rental> {
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id,
     );
-
     if (carUnavailable) {
       throw new AppError('Car is unavailable');
     }
@@ -26,12 +26,17 @@ class CreateRentalUseCase {
     const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
       user_id,
     );
-
     if (rentalOpenToUser) {
       throw new AppError('There is a rental in progress for user');
     }
 
-    console.log(expected_return_date);
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
   }
 }
 
