ddbc4ca790e76421c94076714521926e3d70737a
kielkow <matheuskiel@fiorifer.com.br>
2022-02-28 13:43:59 -0300

    feat: change to  dateProvider methods

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,11 +1,7 @@
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
-
-dayjs.extend(utc);
 
 interface IRequest {
   user_id: string;
@@ -14,7 +10,10 @@ interface IRequest {
 }
 
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider,
+  ) {}
 
   async execute({
     user_id,
@@ -35,15 +34,9 @@ class CreateRentalUseCase {
       throw new AppError('There is a rental in progress for user');
     }
 
-    const dateNowToUTC = dayjs(new Date()).utc().local().format();
-    const expectedReturnDateToUTC = dayjs(expected_return_date)
-      .utc()
-      .local()
-      .format();
-
-    const rentalHours = dayjs(expectedReturnDateToUTC).diff(
-      dateNowToUTC,
-      'hours',
+    const rentalHours = this.dateProvider.compareInHours(
+      this.dateProvider.dateNow(),
+      expected_return_date,
     );
     if (rentalHours < 24) {
       throw new AppError('The rental must have an minimun of 24 hours');
