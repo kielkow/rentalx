c6bc4bab0ff1519162f12f53c9196b6185246e93
kielkow <matheuskiel@fiorifer.com.br>
2022-03-11 10:45:37 -0300

    test: should be able to create a new car

---
+++ src/modules/cars/useCases/createCar/CreateCarController.spec.ts
@@ -0,0 +1,60 @@
+import { hash } from 'bcrypt';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+describe('Create Car Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const password = await hash('admin', 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license ) 
+        values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
+      `,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create a new car', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin',
+    });
+    const { token } = responseToken.body;
+
+    const { body: category } = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Category Supertest',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    const response = await request(app).post('/cars').send({
+      name: 'name',
+      description: 'description',
+      daily_rate: 100,
+      license_plate: 'license_plate',
+      fine_amount: 100,
+      brand: 'brand',
+      category_id: category.id,
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+  });
+});
