a360ecdf2ae0ebbccce2c5e20cf132f4725d3db0
kielkow <matheuskiel@fiorifer.com.br>
2022-03-11 12:31:17 -0300

    test: should be able to list all cars available by controller

---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailablecarsController.spec.ts
@@ -0,0 +1,69 @@
+import { hash } from 'bcrypt';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+describe('List Available Cars Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const password = await hash('admin', 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license ) 
+        values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
+      `,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to list all available cars ', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin',
+    });
+    const { token } = responseToken.body;
+
+    const { body: category } = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Category Supertest',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    const { body: car } = await request(app)
+      .post('/cars')
+      .send({
+        name: 'Car Supertest',
+        description: 'Car Supertest',
+        daily_rate: 100,
+        license_plate: 'Car Supertest',
+        fine_amount: 100,
+        brand: 'Car Supertest',
+        category_id: category.id,
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    const response = await request(app).get('/cars/available');
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].id).toEqual(car.id);
+    expect(response.body[0].name).toEqual('Car Supertest');
+  });
+});
