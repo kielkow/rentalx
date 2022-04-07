6bb38f5078c787841c3c16ee586af01225efac47
kielkow <matheuskiel@fiorifer.com.br>
2022-04-07 11:50:40 -0300

    feat: add method compareInDays to IDateProvider

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -3,6 +3,7 @@ interface IDateProvider {
   dateTomorrow(): Date;
   compareInHours(start_date: Date, end_date: Date): number;
   convertToUTC(date: Date): string;
+  compareInDays(start_date: Date, end_date: Date): number;
 }
 
 export { IDateProvider };
