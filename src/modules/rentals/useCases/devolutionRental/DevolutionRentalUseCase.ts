ff96c17b7aa31d54ec5ba5ef2f2124d276dc21fe
kielkow <matheuskiel@fiorifer.com.br>
2022-04-07 11:49:35 -0300

    feat: create DevolutionRentalUseCase

---
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -0,0 +1,69 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+  id: string;
+  car_id: string;
+}
+
+@injectable()
+class CreateRentalUseCase {
+  constructor(
+    @inject('RentalsRepository')
+    private rentalsRepository: IRentalsRepository,
+
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider,
+
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository,
+  ) {}
+
+  async execute({ id, car_id }: IRequest): Promise<Rental> {
+    let total = 0;
+
+    const rental = await this.rentalsRepository.findById(id);
+    if (!rental) {
+      throw new AppError('Rental does not exists');
+    }
+
+    const car = await this.carsRepository.findById(car_id);
+    if (!car) {
+      throw new AppError('Car does not exists');
+    }
+
+    let daily = this.dateProvider.compareInDays(
+      rental.start_date,
+      this.dateProvider.dateNow(),
+    );
+    if (daily <= 0) {
+      daily = 1;
+    }
+
+    const delay = this.dateProvider.compareInDays(
+      this.dateProvider.dateNow(),
+      rental.expected_return_date,
+    );
+    if (delay > 0) {
+      const calculate_fine = delay * car.fine_amount;
+      total += calculate_fine;
+    }
+
+    total += daily * car.daily_rate;
+
+    rental.total = total;
+    rental.end_date = this.dateProvider.dateNow();
+
+    await this.rentalsRepository.create(rental);
+    await this.carsRepository.updateAvailable(car.id, true);
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
