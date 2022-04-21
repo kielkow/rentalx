7b6e2f18cee6452273a6f8a585aa7a702b3c0d13
kielkow <matheuskiel@fiorifer.com.br>
2022-04-20 22:56:35 -0300

    refact: refact files

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,7 +3,9 @@ import multer from 'multer';
 import { resolve } from 'path';
 
 export default {
-  upload(folder: string) {
+  upload(folder: string): {
+    storage: multer.StorageEngine;
+  } {
     return {
       storage: multer.diskStorage({
         destination: resolve(__dirname, '..', '..', folder),
