3ac56ca90390a3b931d0415f737903ff07dd43f2
kielkow <matheuskiel@fiorifer.com.br>
2022-04-27 12:06:11 -0300

    feat: create auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: 'd2e3a40eae0ed07a8a26c51cbf0d3d34',
+  expires_in_token: '15m',
+  secret_refresh_token: 'b639b432d35503b40646d025c72ac3c9',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
