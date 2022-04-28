7a6a7785ea4c5b81a9caef4ea781d4921c639b27
kielkow <matheuskiel@fiorifer.com.br>
2022-04-28 15:28:14 -0300

    feat: create UsersTokensRepositoryInMemory

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,28 @@
+import { ICreateUserTokenDTO } from '@modules/accounts/dtos/ICreateUserTokenDTO';
+import { UserTokens } from '@modules/accounts/infra/typeorm/entities/UserTokens';
+
+import { IUsersTokensRepository } from '../IUsersTokensRepository';
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+  usersTokens: UserTokens[] = [];
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = new UserTokens();
+
+    Object.assign(userToken, {
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+
+    this.usersTokens.push(userToken);
+
+    return userToken;
+  }
+}
+
+export { UsersTokensRepositoryInMemory };
