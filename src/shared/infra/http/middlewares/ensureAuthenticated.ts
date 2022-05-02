4abe7f2ffd3a789b3e30a4a4a3eee46cfb443cf2
kielkow <matheuskiel@fiorifer.com.br>
2022-05-02 10:27:53 -0300

    refact: refact authenticate middleware and refresh-token usecase

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,7 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import auth from '@config/auth';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
@@ -12,7 +12,7 @@ export async function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction,
-): Promise<Response | void> {
+): Promise<void> {
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
@@ -21,29 +21,15 @@ export async function ensureAuthenticated(
 
   const [, token] = authHeader.split(' ');
 
-  let user_id: string;
   try {
-    const { sub } = verify(
-      token,
-      'd2e3a40eae0ed07a8a26c51cbf0d3d34',
-    ) as IPayload;
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
-    user_id = sub;
-  } catch (error) {
-    throw new AppError('Invalid token', 401);
+    request.user = {
+      id: user_id,
+    };
+
+    next();
+  } catch {
+    throw new AppError('Invalid token!', 401);
   }
-
-  const usersRepository = new UsersRepository();
-
-  const user = await usersRepository.findById(user_id);
-
-  if (!user) {
-    throw new AppError('User does not exists', 401);
-  }
-
-  request.user = {
-    id: user_id,
-  };
-
-  return next();
 }
