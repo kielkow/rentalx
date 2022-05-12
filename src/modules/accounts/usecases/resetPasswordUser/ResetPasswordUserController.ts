9f149e33c933da0568682e08df33d2df58dc3b34
kielkow <matheuskiel@fiorifer.com.br>
2022-05-12 17:04:58 -0300

    feat: create reset user password route

--- src/modules/accounts/usecases/resetPasswordUser/ResetPasswordUserController.ts
+++ src/modules/accounts/usecases/resetPasswordUser/ResetPasswordUserController.ts
@@ -7,6 +7,7 @@ class ResetPasswordUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { token } = request.query;
     const { password } = request.body;
+
     const resetPasswordUserUseCase = container.resolve(
       ResetPasswordUserUseCase,
     );
