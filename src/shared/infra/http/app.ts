385b7df74f0605243075291d19a558a52470611a
kielkow <matheuskiel@fiorifer.com.br>
2022-06-27 14:10:18 -0300

    chore: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swagger from 'swagger-ui-express';
@@ -25,6 +26,7 @@ app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
